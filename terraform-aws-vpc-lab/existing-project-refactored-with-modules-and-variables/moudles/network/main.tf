fead3f51674b42bb626dc7b7cc02ca3a683e3eb5
useruser300 <132722358+useruser300@users.noreply.github.com>
2026-04-21 20:02:41 +0200

    fix: update network module

--- terraform-aws-vpc-lab/existing-project-refactored-with-modules-and-variables/moudles/network/main.tf
+++ terraform-aws-vpc-lab/existing-project-refactored-with-modules-and-variables/moudles/network/main.tf
@@ -4,6 +4,10 @@ cidr_block = var.vpc_cidr
     Name = var.vpc_name
   }
 }
+
+
+
+
 resource "aws_subnet" "public" {
   vpc_id = aws_vpc.main-vpc.id
   cidr_block = var.public_subnet_cidr
@@ -31,25 +35,6 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.main-vpc.id
-  tags = {
-    Name = "public-rt"
-  }
-}
-resource "aws_route" "public_route" {
-  route_table_id = aws_route_table.public_rt.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.igw.id
-}
-
-resource "aws_route_table" "private_rt" {
-  vpc_id = aws_vpc.main-vpc.id
-  tags = {
-    Name = "private-rt"
-  }
-}
-
 resource "aws_eip" "nat_eip" {
   domain = "vpc"
 
@@ -68,4 +53,44 @@ resource "aws_nat_gateway" "nat" {
   }
 }
 
+
+
+
+
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main-vpc.id
+  tags = {
+    Name = "public-rt"
+  }
+}
+resource "aws_route" "public_route" {
+  route_table_id = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public_association" {
+    subnet_id = aws_subnet.public.id
+    route_table_id = aws_route_table.public_rt.id  
+}
+
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main-vpc.id
+  tags = {
+    Name = "private-rt"
+  }
+}
+
+resource "aws_route" "private_route" {
+  route_table_id = aws_route_table.private_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.nat.id
+}
+
+resource "aws_route_table_association" "private_association" {
+    subnet_id = aws_subnet.private.id
+    route_table_id = aws_route_table.private_rt.id  
+}
 
